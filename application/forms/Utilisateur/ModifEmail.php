7e7ad7b4f762a74c2f26ba91a85a47f27a5fde2a
pewho <pewho@gmail.com>
2011-11-21 10:40:14 +0100

    test files

---
+++ application/forms/Utilisateur/ModifEmail.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ModifEmail
+ *
+ * @author pewho
+ */
+class ModifEmail extends Zend_Form
+{
+    //put your code here
+}
+
+?>
