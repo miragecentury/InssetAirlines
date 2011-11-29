73dddb9e345d527cb11e7d46a9ba1c7650597507
Drappier Charles <drappier.charles@live.fr>
2011-11-29 13:37:03 +0100

    Gestion erreur Aeroport + ServExp Ajout de try catch sur tout les acces a la base de donnée

--- application/controllers/AeroportController.php
+++ application/controllers/AeroportController.php
@@ -11,17 +11,27 @@ class AeroportController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $all = Application_Model_Aeroport::getListeAeroportHTML(false);
+        try {
+            $all = Application_Model_Aeroport::getListeAeroportHTML(false);
+        } catch (Zend_Exception $e) {
+            Zend_Registry::get('Log')->log('AeroportController : index : Acces a la base de donnée impossible', Zend_Log::ALERT);
+            return FALSE;
+        }
         if ($all != null)
             $this->view->all = $all;
         else
             $this->view->all = "Erreur dans la base de donnée, 
                 veuillez contacter l'administrateur du site via le formulaire de contact.<br/>";
     }
-    
+
     public function adminAction()
     {
-        $all = Application_Model_Aeroport::getListeAeroportHTML();
+        try {
+            $all = Application_Model_Aeroport::getListeAeroportHTML();
+        } catch (Zend_Exception $e) {
+            Zend_Registry::get('Log')->log('AeroportController : admin : Acces a la base de donnée impossible', Zend_Log::ALERT);
+            return FALSE;
+        }
         if ($all != null)
             $this->view->all = $all;
         else
@@ -32,7 +42,12 @@ class AeroportController extends Zend_Controller_Action
     public function detailAction()
     {
         $Mod = new Application_Model_Aeroport;
-        $item = $Mod->getAeroport($this->getRequest()->getParam('id'));
+        try {
+            $item = $Mod->getAeroport($this->getRequest()->getParam('id'));
+        } catch (Zend_Exception $e) {
+            Zend_Registry::get('Log')->log('AeroportController : detail : Acces a la base de donnée impossible', Zend_Log::ALERT);
+            return FALSE;
+        }
         if ($item != null) {
             $this->view->item = $item->getAeroportHTML();
         } else {
@@ -46,11 +61,21 @@ class AeroportController extends Zend_Controller_Action
         $request = $this->getRequest();
         if ($request->getParam('ok') === "ok") {
             $Mod = new Application_Model_Aeroport;
-            $Mod->delAeroport($request->getParam('id'));
+            try {
+                $Mod->delAeroport($request->getParam('id'));
+            } catch (Zend_Exception $e) {
+                Zend_Registry::get('Log')->log('AeroportController : del : Acces a la base de donnée impossible', Zend_Log::ALERT);
+                return FALSE;
+            }
             $this->_redirect('/Aeroport/admin');
         } else {
             $Mod = new Application_Model_Aeroport;
-            $item = $Mod->getAeroport($request->getParam('id'));
+            try {
+                $item = $Mod->getAeroport($request->getParam('id'));
+            } catch (Zend_Exception $e) {
+                Zend_Registry::get('Log')->log('AeroportController : del : Acces a la base de donnée impossible', Zend_Log::ALERT);
+                return FALSE;
+            }
             if ($item != null) {
                 $this->view->item = $item->getAeroportHTML();
             } else {
@@ -71,7 +96,12 @@ class AeroportController extends Zend_Controller_Action
             $item->set_labelAeroport($request->getParam('label'))
                     ->set_labelPays($request->getParam('pays'))
                     ->set_labelVille($request->getParam('ville'));
-            $item->addAeroport();
+            try {
+                $item->addAeroport();
+            } catch (Zend_Exception $e) {
+                Zend_Registry::get('Log')->log('AeroportController : new : Acces a la base de donnée impossible', Zend_Log::ALERT);
+                return FALSE;
+            }
             $this->_redirect('/Aeroport/admin');
         }
     }
@@ -81,21 +111,31 @@ class AeroportController extends Zend_Controller_Action
         $request = $this->getRequest();
         if ($request->getParam('submit') != "Valider") {
             $item = new Application_Model_Aeroport;
-            $item = $item->getAeroport($request->getParam('id'));
+            try {
+                $item = $item->getAeroport($request->getParam('id'));
+            } catch (Zend_Exception $e) {
+                Zend_Registry::get('Log')->log('AeroportController : upd : Acces a la base de donnée impossible', Zend_Log::ALERT);
+                return FALSE;
+            }
+            $form = new Application_Form_Aeroport_Aeroport();
             if ($item != null) {
-                $form = new Application_Form_Aeroport_Aeroport();
                 $form->getElement('label')->setValue($item->get_labelAeroport());
                 $form->getElement('pays')->setValue($item->get_labelPays());
                 $form->getElement('ville')->setValue($item->get_labelVille());
-                $this->view->form = $form;
             }
+            $this->view->form = $form;
         } else {
             $item = new Application_Model_Aeroport;
             $item->set_noAeroport($request->getParam('id'))
                     ->set_labelAeroport($request->getParam('label'))
                     ->set_labelPays($request->getParam('pays'))
                     ->set_labelVille($request->getParam('ville'));
-            $item->addAeroport();
+            try {
+                $item->addAeroport();
+            } catch (Zend_Exception $e) {
+                Zend_Registry::get('Log')->log('AeroportController : upd : Acces a la base de donnée impossible', Zend_Log::ALERT);
+                return FALSE;
+            }
             $this->_redirect('/Aeroport/admin');
         }
     }
