defd540f72760ead3a04ec85570292e982739688
pewho <pewho@gmail.com>
2011-12-03 23:37:55 +0100

    Action modLigne
    -> ajout de l'action, debug
    -> ajout de la view

--- application/modules/ServStrategique/controllers/GestvolController.php
+++ application/modules/ServStrategique/controllers/GestvolController.php
@@ -40,7 +40,7 @@ class ServStrategique_GestvolController extends Zend_Controller_Action
             $addVolForm->getValue('aeroDep') == $addVolForm->getValue('aeroAtt')) {
             $this->view->errorMessage = "L'aeroport de départ ne peut être le même
                 que celui d'arrivée";
-        //sinon si le formulaire est valide
+            //sinon si le formulaire est valide
         } else if (!empty($_POST) && $addVolForm->isValid($_POST)) {
             //setter
             $ligne->set_labelAeroportAtte($addVolForm->getValue('aeroAtt'))
@@ -52,8 +52,6 @@ class ServStrategique_GestvolController extends Zend_Controller_Action
 
             //enregistrement
             $reussite = $ligne->addLigne();
-            var_dump($reussite);
-            var_dump($ligne);
             $session = new Zend_Session_Namespace('Redirect');
             if ($reussite) {
                 $session->message = 'La ligne a bien été ajouté !';
@@ -62,7 +60,7 @@ class ServStrategique_GestvolController extends Zend_Controller_Action
             } else {
                 $session->message = "Erreur lors de l'ajout de la ligne !";
                 $session->redirection = '/ServStrategique/gestvol';
-                //$this->_redirect('/redirection/fail');
+                $this->_redirect('/redirection/fail');
             }
         } else {
             $this->view->errorMessage = 'Le formulaire est invalide !';
@@ -71,7 +69,63 @@ class ServStrategique_GestvolController extends Zend_Controller_Action
 
     public function updvolAction()
     {
+        //Chargement de la form associé
+        $addVolForm = new ServStrategique_Form_addLigne();
+        $this->view->form = $addVolForm;
+        $session = new Zend_Session_Namespace('Redirect');
 
+        //initialisation d'un objet ligne
+        $id = (int) $this->getRequest()->getParam('id');
+        $ligne = ServStrategique_Model_Ligne::getLigne($id);
+
+        //si la ligne n'existe pas
+        if (is_null($ligne)){
+            $session->message = "Erreur : la ligne n'existe pas !";
+            $session->redirection = '/ServStrategique/gestvol';
+            $this->_redirect('/redirection/fail');
+        }
+
+        //Préremplissage de la form
+        $addVolForm->setDefaults(array(
+            'aeroDep' => $ligne->get_labelAeroportDeco(),
+            'aeroAtt' => $ligne->get_labelAeroportAtte(),
+            'jour' => $ligne->get_jours(),
+            'semaine' => $ligne->get_semaines(),
+            'mois' => $ligne->get_mois(),
+            'annee' => $ligne->get_annees()
+        ));
+
+        //si le formulaire est valide et en cas d'aeroport de depart = aeroport d'arrivé
+        if (!empty($_POST) && $addVolForm->isValid($_POST) &&
+            $addVolForm->getValue('aeroDep') == $addVolForm->getValue('aeroAtt')) {
+            $this->view->errorMessage = "L'aeroport de départ ne peut être le même
+                que celui d'arrivée";
+            //sinon si le formulaire est valide
+        } else if (!empty($_POST) && $addVolForm->isValid($_POST)) {
+            //setter
+            $ligne->set_labelAeroportAtte($addVolForm->getValue('aeroAtt'))
+                ->set_labelAeroportDeco($addVolForm->getValue('aeroDep'))
+                ->set_jours($addVolForm->getValue('jour'))
+                ->set_semaines($addVolForm->getValue('semaine'))
+                ->set_mois($addVolForm->getValue('mois'))
+                ->set_annees($addVolForm->getValue('annee'));
+
+            //enregistrement
+            $reussite = $ligne->addLigne();
+
+            //redirection
+            if ($reussite) {
+                $session->message = 'La ligne a bien été ajouté !';
+                $session->redirection = '/ServStrategique/gestvol';
+                $this->_redirect('/redirection/success');
+            } else {
+                $session->message = "Erreur lors de l'ajout de la ligne !";
+                $session->redirection = '/ServStrategique/gestvol';
+                $this->_redirect('/redirection/fail');
+            }
+        } else {
+            $this->view->errorMessage = 'Le formulaire est invalide !';
+        }
     }
 
     public function delvolAction()
