5b44518a20d4b3b91373959497b62701fcbbe2a9
mylune <renaud.camille02@gmail.com>
2011-12-05 09:38:52 +0100

    Ajout des ACL de DRH

--- application/modules/ServDRH/controllers/GestpiloteController.php
+++ application/modules/ServDRH/controllers/GestpiloteController.php
@@ -5,7 +5,16 @@ class ServDRH_GestpiloteController extends Zend_Controller_Action {
     public function init() {
         $this->view->setLfiProtection(false);
         $this->view->render('../../../../views/scripts/user/_sidebar.phtml');
-        $this->view->render('../../../../views/scripts/user/_login.phtml');        
+        $this->view->render('../../../../views/scripts/user/_login.phtml');  
+        $this->_acl = Zend_Registry::get('Acl');
+        //ACL
+        $authSession = new Zend_Session_Namespace('Zend_Auth');
+        if (!$this->_acl->isAllowed($authSession->role, 'Mod_Serv_DRH')) {
+            $session = new Zend_Session_Namespace('Redirect');
+            $session->message = "Vous n'avez pas les droits pour acceder à ce service";
+            $session->redirection = "/";
+            $this->_redirect('/redirection/fail');
+        }
     }
 
     public function indexAction() {        
