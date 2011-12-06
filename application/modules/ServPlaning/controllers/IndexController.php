ffba8ae2c1f3167067d9105ed209eef94bb9d9a9
pewho <pewho@gmail.com>
2011-12-06 14:22:21 +0100

    Commentage du paginator exemple

--- application/modules/ServPlaning/controllers/IndexController.php
+++ application/modules/ServPlaning/controllers/IndexController.php
@@ -13,12 +13,13 @@ class ServPlaning_IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $liste = Application_Model_Personne::getListePersonne();
-        $page = Zend_Paginator::factory($liste);
-        $page->setPageRange(3);
-        $page->setCurrentPageNumber($this->_getParam('page', 1));
-        $page->setItemCountPerPage($this->_getParam('par', 5));
-        $this->view->listeMembres = $page;
+//PAGINATOR EXEMPLE
+//        $liste = Application_Model_Personne::getListePersonne();
+//        $page = Zend_Paginator::factory($liste);
+//        $page->setPageRange(3);
+//        $page->setCurrentPageNumber($this->_getParam('page', 1));
+//        $page->setItemCountPerPage($this->_getParam('par', 10));
+//        $this->view->listeMembres = $page;
     }
 
 
