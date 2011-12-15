6c9c11627ea876499795736c6149dc6ec1a568fb
pewho <pewhoo@gmail.com>
2011-12-15 09:34:43 +0100

    BASEURL -> SERVMaintenance Controllers OK

--- application/modules/ServMaintenance/controllers/GesttachemaintenanceController.php
+++ application/modules/ServMaintenance/controllers/GesttachemaintenanceController.php
@@ -1,76 +1,80 @@
 <?php
 
-class ServMaintenance_GesttachemaintenanceController extends Zend_Controller_Action {
+class ServMaintenance_GesttachemaintenanceController extends Zend_Controller_Action
+{
 
-    public function init() {
-        $this->view->setLfiProtection(false);
-        $this->view->render('../../../../views/scripts/user/_sidebar.phtml');
-        $this->view->render('../../../../views/scripts/user/_login.phtml');
-        $this->view->render('sidebar/_homeMaintenanceSideBar.phtml');
-        $this->view->render('sidebar/_homeGestTacheMaintenanceSideBar.phtml');
+    public function init()
+    {
+        $this->view->setLfiProtection( false );
+        $this->view->render( '../../../../views/scripts/user/_sidebar.phtml' );
+        $this->view->render( '../../../../views/scripts/user/_login.phtml' );
+        $this->view->render( 'sidebar/_homeMaintenanceSideBar.phtml' );
+        $this->view->render( 'sidebar/_homeGestTacheMaintenanceSideBar.phtml' );
     }
 
-    public function indexAction() {
-        $this->view->nbAff = array();
+    public function indexAction()
+    {
+        $this->view->nbAff = array( );
         //Tâche en cours
 
-        $this->view->nbAff['encoursP'] = ServMaintenance_Model_TacheMaintenance::nbMaintenanceEnCoursAujourdhui(ServMaintenance_Model_TacheMaintenance::TACHE_PREVENTIVE);
-        $this->view->nbAff['encoursU'] = ServMaintenance_Model_TacheMaintenance::nbMaintenanceEnCoursAujourdhui(ServMaintenance_Model_TacheMaintenance::TACHE_URGENTE);
+        $this->view->nbAff[ 'encoursP' ] = ServMaintenance_Model_TacheMaintenance::nbMaintenanceEnCoursAujourdhui( ServMaintenance_Model_TacheMaintenance::TACHE_PREVENTIVE );
+        $this->view->nbAff[ 'encoursU' ] = ServMaintenance_Model_TacheMaintenance::nbMaintenanceEnCoursAujourdhui( ServMaintenance_Model_TacheMaintenance::TACHE_URGENTE );
 
         $this->view->lstTacheAPlanifier = ServMaintenance_Model_TacheMaintenance::getMaintenanceUrgenteAPlanifier();
 
         $this->view->lstTacheplanifier = ServMaintenance_Model_TacheMaintenance::findAllPlanifierAtCurrentTime();
     }
 
-    public function aplanifierAction() {
+    public function aplanifierAction()
+    {
         $form = new ServMaintenance_Form_TacheMaintenance_planifier();
-        $noAvion = $this->getRequest()->getParam('noavion');
-        $noTypeMaintenance = $this->getRequest()->getParam('notypemaintenance');
+        $noAvion = $this->getRequest()->getParam( 'noavion' );
+        $noTypeMaintenance = $this->getRequest()->getParam( 'notypemaintenance' );
 
-        $Avion = ServMaintenance_Model_Avion::findOne($noAvion);
-        $TypeMaintenance = ServMaintenance_Model_TypeMaintenance::findOne($noTypeMaintenance);
-        if ($Avion instanceof ServMaintenance_Model_Avion && $TypeMaintenance instanceof ServMaintenance_Model_TypeMaintenance && $Avion->get_noModele() == $TypeMaintenance->get_noModele()) {
+        $Avion = ServMaintenance_Model_Avion::findOne( $noAvion );
+        $TypeMaintenance = ServMaintenance_Model_TypeMaintenance::findOne( $noTypeMaintenance );
+        if ( $Avion instanceof ServMaintenance_Model_Avion && $TypeMaintenance instanceof ServMaintenance_Model_TypeMaintenance && $Avion->get_noModele() == $TypeMaintenance->get_noModele() ) {
             $this->view->label = '<span style="color: red;"> Urgente</span>';
-            $form->setForUrg($noAvion, $noTypeMaintenance);
+            $form->setForUrg( $noAvion, $noTypeMaintenance );
             $this->view->affAvionTypeMaintenance = ' - ' . $Avion->get_noAvion() . ': ' . $Avion->get_label() . ':' . $TypeMaintenance->get_label() . ':' . ' - ';
         } else {
             $this->view->label = '<span style="color: green;"> Préventive</span>';
         }
         //parse_str($_POST['AvionTypeMaintenance'])
-        if (isset($_POST) && !empty($_POST)) {
-            if ($form->isValid($_POST)) {
-                if (!isset($noAvion) && !isset($noTypeMaintenance)) {
-                    parse_str($_POST['AvionTypeMaintenance']);
+        if ( isset( $_POST ) && !empty( $_POST ) ) {
+            if ( $form->isValid( $_POST ) ) {
+                if ( !isset( $noAvion ) && !isset( $noTypeMaintenance ) ) {
+                    parse_str( $_POST[ 'AvionTypeMaintenance' ] );
                 }
-                if (isset($noAvion) && isset($noTypeMaintenance)) {
+                if ( isset( $noAvion ) && isset( $noTypeMaintenance ) ) {
 
-                    $Avion = ServMaintenance_Model_Avion::findOne($noAvion);
-                    $TypeMaintenance = ServMaintenance_Model_TypeMaintenance::findOne($noTypeMaintenance);
-                    if ($Avion instanceof ServMaintenance_Model_Avion && $TypeMaintenance instanceof ServMaintenance_Model_TypeMaintenance && $Avion->get_noModele() == $TypeMaintenance->get_noModele()) {
-                        $dateDebut = new DateTime($_POST['dateDebut']);
-                        $dateDebut->setTime(0, 0, 0);
-                        $dateEnd = new DateTime($dateDebut->format(DATE_ATOM));
-                        $dateEnd->modify('+' . $TypeMaintenance->get_dureeMaintenance() . ' day');
-                        if ($dateDebut->format(DATE_ATOM) > date(DATE_ATOM)) {
-                            $Maintenance = ServMaintenance_Model_TacheMaintenance::findAllByAvionAtDateTimeInterval($dateDebut, $dateEnd, $noAvion);
-                            if (count($Maintenance) > 0) {
+                    $Avion = ServMaintenance_Model_Avion::findOne( $noAvion );
+                    $TypeMaintenance = ServMaintenance_Model_TypeMaintenance::findOne( $noTypeMaintenance );
+                    if ( $Avion instanceof ServMaintenance_Model_Avion && $TypeMaintenance instanceof ServMaintenance_Model_TypeMaintenance && $Avion->get_noModele() == $TypeMaintenance->get_noModele() ) {
+                        $dateDebut = new DateTime( $_POST[ 'dateDebut' ] );
+                        $dateDebut->setTime( 0, 0, 0 );
+                        $dateEnd = new DateTime( $dateDebut->format( DATE_ATOM ) );
+                        $dateEnd->modify( '+' . $TypeMaintenance->get_dureeMaintenance() . ' day' );
+                        if ( $dateDebut->format( DATE_ATOM ) > date( DATE_ATOM ) ) {
+                            $Maintenance = ServMaintenance_Model_TacheMaintenance::findAllByAvionAtDateTimeInterval( $dateDebut, $dateEnd, $noAvion );
+                            if ( count( $Maintenance ) > 0 ) {
                                 $this->view->message = 'Il y a déjà une Maintenance pour cette Avion a cette période!';
                                 $this->view->form = $form;
                             } else {
-                                if ($dateDebut->format(DATE_ATOM) < ServPlaning_Model_Vol::getSemaineAheadFromCurrent(5)) {
+                                if ( $dateDebut->format( DATE_ATOM ) < ServPlaning_Model_Vol::getSemaineAheadFromCurrent( 5 ) ) {
                                     //implication pour Vol
                                 } else {
                                     $tacheMaintenance = new ServMaintenance_Model_TacheMaintenance();
-                                    $tacheMaintenance->set_dateDebut($dateDebut->format(DATE_ATOM));
-                                    $tacheMaintenance->set_dateFin($dateEnd->format(DATE_ATOM));
-                                    if (isset($this->view->affAvionTypeMaintenance)) {
-                                        $tacheMaintenance->set_etat(ServMaintenance_Model_TacheMaintenance::TACHE_URGENTE);
+                                    $tacheMaintenance->set_dateDebut( $dateDebut->format( DATE_ATOM ) );
+                                    $tacheMaintenance->set_dateFin( $dateEnd->format( DATE_ATOM ) );
+                                    if ( isset( $this->view->affAvionTypeMaintenance ) ) {
+                                        $tacheMaintenance->set_etat( ServMaintenance_Model_TacheMaintenance::TACHE_URGENTE );
                                     } else {
-                                        $tacheMaintenance->set_etat(ServMaintenance_Model_TacheMaintenance::TACHE_PREVENTIVE);
+                                        $tacheMaintenance->set_etat( ServMaintenance_Model_TacheMaintenance::TACHE_PREVENTIVE );
                                     }
-                                    $tacheMaintenance->set_noTypeMaintenance($TypeMaintenance->get_noTypeMaintenance());
-                                    $tacheMaintenance->set_noAvion($Avion->get_noAvion());
-                                    $tacheMaintenance->set_retard(0);
+                                    $tacheMaintenance->set_noTypeMaintenance( $TypeMaintenance->get_noTypeMaintenance() );
+                                    $tacheMaintenance->set_noAvion( $Avion->get_noAvion() );
+                                    $tacheMaintenance->set_retard( 0 );
                                     $tacheMaintenance->save();
                                 }
                             }
@@ -95,25 +99,28 @@ class ServMaintenance_GesttachemaintenanceController extends Zend_Controller_Act
         }
     }
 
-    public function completerAction() {
-        $id = $this->getRequest()->getParam('id');
-        $form = new ServMaintenance_Form_TacheMaintenance_completer($id);
-        if (isset($_POST) && !empty($_POST)) {
-            if ($form->isValid($_POST)) {
-                $maintenance = ServMaintenance_Model_TacheMaintenance::findOne($id);
-                if ($maintenance instanceof ServMaintenance_Model_TacheMaintenance) {
-                    $maintenance->set_rapport(base64_encode($_POST['rapport']));
-                    if ($_POST['retard'] > 0) {
+    public function completerAction()
+    {
+        $id = $this->getRequest()->getParam( 'id' );
+        $form = new ServMaintenance_Form_TacheMaintenance_completer( $id );
+        if ( isset( $_POST ) && !empty( $_POST ) ) {
+            if ( $form->isValid( $_POST ) ) {
+                $maintenance = ServMaintenance_Model_TacheMaintenance::findOne( $id );
+                if ( $maintenance instanceof ServMaintenance_Model_TacheMaintenance ) {
+                    $maintenance->set_rapport( base64_encode( $_POST[ 'rapport' ] ) );
+                    if ( $_POST[ 'retard' ] > 0 ) {
                         $this->view->message = 'Le retard n\'est pas encore implémenté';
                         $this->view->form = $form;
                     } else {
                         $maintenance->save();
                         $this->view->message = 'Rapport Transmis';
-                        $this->getResponse()->setHeader('refresh', '1,url=/ServMaintenance/Gesttachemaintenance');
+                        $this->getResponse()->setHeader( 'refresh', '1,url=' . Zend_Registry::get( 'BaseUrl' ) .
+                            '/ServMaintenance/Gesttachemaintenance' );
                     }
                 } else {
                     $this->view->message = 'Id de la Tâche de Maintenance Incorrecte';
-                    $this->getResponse()->setHeader('refresh', '1,url=/ServMaintenance/Gesttachemaintenance');
+                    $this->getResponse()->setHeader( 'refresh', '1,url=' . Zend_Registry::get( 'BaseUrl' ) .
+                        '/ServMaintenance/Gesttachemaintenance' );
                 }
             } else {
                 $this->view->message = 'Données Incohérentes';
@@ -125,23 +132,26 @@ class ServMaintenance_GesttachemaintenanceController extends Zend_Controller_Act
         }
     }
 
-    public function creerAction() {
-        if (isset($_POSt) && !empty($_POST)) {
-            
+    public function creerAction()
+    {
+        if ( isset( $_POSt ) && !empty( $_POST ) ) {
+
         } else {
-            
+
         }
     }
 
-    public function cloturerAction() {
-        if (isset($_POST) && !empty($_POST)) {
-            
+    public function cloturerAction()
+    {
+        if ( isset( $_POST ) && !empty( $_POST ) ) {
+
         } else {
-            
+
         }
     }
 
-    public function encoursAction() {
+    public function encoursAction()
+    {
         $this->view->lstencours = ServMaintenance_Model_TacheMaintenance::findAllAtCurrentTime();
     }
 
--- application/modules/ServMaintenance/controllers/GesttypemaintenanceController.php
+++ application/modules/ServMaintenance/controllers/GesttypemaintenanceController.php
@@ -1,33 +1,36 @@
 <?php
 
-class ServMaintenance_GesttypemaintenanceController extends Zend_Controller_Action {
+class ServMaintenance_GesttypemaintenanceController extends Zend_Controller_Action
+{
 
-    public function init() {
-        $this->view->setLfiProtection(false);
-        $this->view->render('../../../../views/scripts/user/_sidebar.phtml');
-        $this->view->render('../../../../views/scripts/user/_login.phtml');
-        $this->view->render('sidebar/_homeMaintenanceSideBar.phtml');
-        $this->view->render('sidebar/_homeGestTypeMaintenanceSideBar.phtml');
+    public function init()
+    {
+        $this->view->setLfiProtection( false );
+        $this->view->render( '../../../../views/scripts/user/_sidebar.phtml' );
+        $this->view->render( '../../../../views/scripts/user/_login.phtml' );
+        $this->view->render( 'sidebar/_homeMaintenanceSideBar.phtml' );
+        $this->view->render( 'sidebar/_homeGestTypeMaintenanceSideBar.phtml' );
     }
 
-    public function indexAction() {
+    public function indexAction()
+    {
         $modeles = ServMaintenance_Model_Modele::getAll();
-        $ListeModeles = array();
-        $ListeConstructeur = array();
-        $ListeTypesMaintenance = array();
+        $ListeModeles = array( );
+        $ListeConstructeur = array( );
+        $ListeTypesMaintenance = array( );
 
         $ListeConstructeurRaw = ServMaintenance_Model_Constructeur::GetAll();
-        foreach ($ListeConstructeurRaw as $Constructeur) {
-            if ($Constructeur instanceof ServMaintenance_Model_Constructeur) {
-                $ListeConstructeur[$Constructeur->get_noConstructeur()] = $Constructeur;
+        foreach ( $ListeConstructeurRaw as $Constructeur ) {
+            if ( $Constructeur instanceof ServMaintenance_Model_Constructeur ) {
+                $ListeConstructeur[ $Constructeur->get_noConstructeur() ] = $Constructeur;
             }
         }
 
-        foreach ($modeles as $modele) {
-            if ($modele instanceof ServMaintenance_Model_Modele) {
-                $TypesMaintenances = ServMaintenance_Model_TypeMaintenance::findAllByModele($modele->get_noModele());
-                $ListeModeles[$modele->get_noModele()] = $modele;
-                $ListeTypesMaintenance[$modele->get_noModele()] = $TypesMaintenances;
+        foreach ( $modeles as $modele ) {
+            if ( $modele instanceof ServMaintenance_Model_Modele ) {
+                $TypesMaintenances = ServMaintenance_Model_TypeMaintenance::findAllByModele( $modele->get_noModele() );
+                $ListeModeles[ $modele->get_noModele() ] = $modele;
+                $ListeTypesMaintenance[ $modele->get_noModele() ] = $TypesMaintenances;
             }
         }
         $this->view->ListeModeles = $ListeModeles;
@@ -35,21 +38,23 @@ class ServMaintenance_GesttypemaintenanceController extends Zend_Controller_Acti
         $this->view->ListeConstructeur = $ListeConstructeur;
     }
 
-    public function newtypemaintenanceAction() {
-        if (isset($_POST) && !empty($_POST)) {
+    public function newtypemaintenanceAction()
+    {
+        if ( isset( $_POST ) && !empty( $_POST ) ) {
             $form = new ServMaintenance_Form_TypeMaintenance();
-            if ($form->isValid($_POST)) {
-                if (ServMaintenance_Model_Modele::findOne($_POST['noModele']) instanceof ServMaintenance_Model_Modele) {
+            if ( $form->isValid( $_POST ) ) {
+                if ( ServMaintenance_Model_Modele::findOne( $_POST[ 'noModele' ] ) instanceof ServMaintenance_Model_Modele ) {
                     $TypeMaintenance = new ServMaintenance_Model_TypeMaintenance();
-                    $TypeMaintenance->set_label($_POST['label'])
-                            ->set_noModele($_POST['noModele'])
-                            ->set_periode($_POST['periode'])
-                            ->set_dureeMaintenance($_POST['dureeMaintenance']);
+                    $TypeMaintenance->set_label( $_POST[ 'label' ] )
+                        ->set_noModele( $_POST[ 'noModele' ] )
+                        ->set_periode( $_POST[ 'periode' ] )
+                        ->set_dureeMaintenance( $_POST[ 'dureeMaintenance' ] );
                     try {
                         $TypeMaintenance->save();
                         $this->view->message = 'Ajout du Type de Maintenance Confirmé';
-                        $this->getResponse()->setHeader('refresh', '2,url=/ServMaintenance/Gesttypemaintenance');
-                    } catch (Exception $e) {
+                        $this->getResponse()->setHeader( 'refresh', '2,url=' . Zend_Registry::get( 'BaseUrl' ) .
+                            '/ServMaintenance/Gesttypemaintenance' );
+                    } catch ( Exception $e ) {
                         $this->view->message = 'Erreur Impromptue' . $e->getMessage() . ':' . $e->getPrevious()->getMessage();
                         $this->view->form = $form;
                     }
@@ -62,12 +67,12 @@ class ServMaintenance_GesttypemaintenanceController extends Zend_Controller_Acti
                 $this->view->form = $form;
             }
         } else {
-            $noModele = $this->getRequest()->getParam('noModele');
-            if ($noModele === null) {
+            $noModele = $this->getRequest()->getParam( 'noModele' );
+            if ( $noModele === null ) {
                 $this->view->form = new ServMaintenance_Form_TypeMaintenance();
             } else {
-                if (preg_match('#^[0-9]{1,10}$#', $noModele) && $noModele > 0) {
-                    $this->view->form = new ServMaintenance_Form_TypeMaintenance($noModele);
+                if ( preg_match( '#^[0-9]{1,10}$#', $noModele ) && $noModele > 0 ) {
+                    $this->view->form = new ServMaintenance_Form_TypeMaintenance( $noModele );
                 } else {
                     $this->view->form = new ServMaintenance_Form_TypeMaintenance();
                 }
@@ -75,25 +80,27 @@ class ServMaintenance_GesttypemaintenanceController extends Zend_Controller_Acti
         }
     }
 
-    public function updtypemaintenanceAction() {
-        $noTypeMaintenance = $this->getRequest()->getParam('noTypeMaintenance');
-        if (isset($_POST) && !empty($_POST)) {
+    public function updtypemaintenanceAction()
+    {
+        $noTypeMaintenance = $this->getRequest()->getParam( 'noTypeMaintenance' );
+        if ( isset( $_POST ) && !empty( $_POST ) ) {
             if (
-                    preg_match('#^[0-9]{1,10}$#', $noTypeMaintenance) && $noTypeMaintenance > 0 &&
-                    ($TypeMaintenance = ServMaintenance_Model_TypeMaintenance::findOne($noTypeMaintenance)) instanceof ServMaintenance_Model_TypeMaintenance
+                preg_match( '#^[0-9]{1,10}$#', $noTypeMaintenance ) && $noTypeMaintenance > 0 &&
+                ($TypeMaintenance = ServMaintenance_Model_TypeMaintenance::findOne( $noTypeMaintenance )) instanceof ServMaintenance_Model_TypeMaintenance
             ) {
                 $form = new ServMaintenance_Form_TypeMaintenance();
-                $form->updateForm($TypeMaintenance);
-                if ($form->isValid($_POST)) {
-                    $TypeMaintenance->set_dureeMaintenance($_POST['dureeMaintenance']);
-                    $TypeMaintenance->set_periode($_POST['periode']);
+                $form->updateForm( $TypeMaintenance );
+                if ( $form->isValid( $_POST ) ) {
+                    $TypeMaintenance->set_dureeMaintenance( $_POST[ 'dureeMaintenance' ] );
+                    $TypeMaintenance->set_periode( $_POST[ 'periode' ] );
                     try {
                         $TypeMaintenance->save();
                         $this->view->message = 'Modification Effectuée';
-                        $this->getResponse()->setHeader('refresh', '2,url=/ServMaintenance/Gesttypemaintenance');
-                    } catch (Exception $e) {
-                        $this->view->message = 'Erreur Inopinée : '.$e->getMessage().' : '.$e->getPrevious()->getMessage();
-                    $this->view->form = $form;
+                        $this->getResponse()->setHeader( 'refresh', '2,url=' . Zend_Registry::get( 'BaseUrl' ) .
+                            '/ServMaintenance/Gesttypemaintenance' );
+                    } catch ( Exception $e ) {
+                        $this->view->message = 'Erreur Inopinée : ' . $e->getMessage() . ' : ' . $e->getPrevious()->getMessage();
+                        $this->view->form = $form;
                     }
                 } else {
                     $this->view->message = 'Données Incorrectes';
@@ -101,28 +108,32 @@ class ServMaintenance_GesttypemaintenanceController extends Zend_Controller_Acti
                 }
             } else {
                 $this->view->message = 'Type de Maintenance Incorrecte!';
-                $this->getResponse()->setHeader('refresh', '2,url=/ServMaintenance/Gesttypemaintenance');
+                $this->getResponse()->setHeader( 'refresh', '2,url=' . Zend_Registry::get( 'BaseUrl' ) .
+                    '/ServMaintenance/Gesttypemaintenance' );
             }
         } else {
-            if (preg_match('#^[0-9]{1,10}$#', $noTypeMaintenance) && $noTypeMaintenance > 0) {
-                if (($TypeMaintenance = ServMaintenance_Model_TypeMaintenance::findOne($noTypeMaintenance)) instanceof ServMaintenance_Model_TypeMaintenance) {
+            if ( preg_match( '#^[0-9]{1,10}$#', $noTypeMaintenance ) && $noTypeMaintenance > 0 ) {
+                if ( ($TypeMaintenance = ServMaintenance_Model_TypeMaintenance::findOne( $noTypeMaintenance )) instanceof ServMaintenance_Model_TypeMaintenance ) {
                     $form = new ServMaintenance_Form_TypeMaintenance();
-                    $form->updateForm($TypeMaintenance);
+                    $form->updateForm( $TypeMaintenance );
 
                     $this->view->form = $form;
                 } else {
                     $this->view->message = 'Type de Maintenance Incorrecte!';
-                    $this->getResponse()->setHeader('refresh', '2,url=/ServMaintenance/Gesttypemaintenance');
+                    $this->getResponse()->setHeader( 'refresh', '2,url=' . Zend_Registry::get( 'BaseUrl' ) .
+                        '/ServMaintenance/Gesttypemaintenance' );
                 }
             } else {
                 $this->view->message = 'Type de Maintenance Incorrecte!';
-                $this->getResponse()->setHeader('refresh', '2,url=/ServMaintenance/Gesttypemaintenance');
+                $this->getResponse()->setHeader( 'refresh', '2,url=' . Zend_Registry::get( 'BaseUrl' ) .
+                    '/ServMaintenance/Gesttypemaintenance' );
             }
         }
     }
 
-    public function deltypemaintenanceAction() {
-        
+    public function deltypemaintenanceAction()
+    {
+
     }
 
 }
